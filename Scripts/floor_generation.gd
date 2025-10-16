e6045127e271ef92631ba38b661ff62cdca35d1c
n0o0b090lv <ernestspursw@gmail.com>
2025-10-16 19:24:30 +0300

    Well square is a square XD

--- Scripts/floor_generation.gd
+++ Scripts/floor_generation.gd
@@ -15,33 +15,11 @@ func gen_floor():
 	var normals = PackedVector3Array()
 	var indices = PackedInt32Array()
 
-	var thisrow = 0
-	var prevrow = 0
-	var point = 0
+	_add_quad(Transform3D(
+		Basis.looking_at(Vector3.UP, Vector3.BACK) * 2,
+		Vector3(1, 0, 0)
+	), verts, uvs, normals, indices)
 
-	# Loop over rings.
-	for i in range(floor_size + 1):
-		for j in range(floor_size + 1):
-			var vert = Vector3(i, 0, j)
-			verts.append(vert)
-			normals.append(vert.normalized())
-			uvs.append(Vector2(1, 1))
-			point += 1
-
-			# Create triangles in ring using indices.
-			if i > 0 and j > 0:
-				indices.append(prevrow + j - 1)
-				indices.append(prevrow + j)
-				indices.append(thisrow + j - 1)
-
-				indices.append(prevrow + j)
-				indices.append(thisrow + j)
-				indices.append(thisrow + j - 1)
-
-		prevrow = thisrow
-		thisrow = point
-	
-	mesh.create_trimesh_collision()
 	# Assign arrays to surface array.
 	surface_array[Mesh.ARRAY_VERTEX] = verts
 	surface_array[Mesh.ARRAY_TEX_UV] = uvs
@@ -51,3 +29,37 @@ func gen_floor():
 	# Create mesh surface from mesh array.
 	# No blendshapes, lods, or compression used.
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+
+#https://gamedev.net/blogs/entry/2277691-procedural-geometry-in-godot/
+func _add_quad(xform: Transform3D, verts: PackedVector3Array, uvs: PackedVector2Array, normals: PackedVector3Array, triangles: PackedInt32Array) -> void:
+	var index := verts.size()
+	
+	# corners before transforming
+	var vert1 := Vector3(-.5, -.5, 0)
+	var vert2 := Vector3(-.5, .5, 0)
+	var vert3 := Vector3(.5, .5, 0)
+	var vert4 := Vector3(.5, -.5, 0)
+	
+	verts.append(xform * vert1)
+	verts.append(xform * vert2)
+	verts.append(xform * vert3)
+	verts.append(xform * vert4)
+	
+	uvs.append(Vector2(1, 0))
+	uvs.append(Vector2(1, 1))
+	uvs.append(Vector2(0, 1))
+	uvs.append(Vector2(0, 0))
+	
+	var normal := xform * Vector3.FORWARD;
+	normals.append(normal)
+	normals.append(normal)
+	normals.append(normal)
+	normals.append(normal)
+	
+	triangles.append(index + 2)
+	triangles.append(index + 1)
+	triangles.append(index)
+	
+	triangles.append(index + 3)
+	triangles.append(index + 2)
+	triangles.append(index)
