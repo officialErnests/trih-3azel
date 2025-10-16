f52b3cb3b81dea03b570b3bc3058f82d78656545
n0o0b090lv <ernestspursw@gmail.com>
2025-10-16 18:52:58 +0300

    Uhhhh still wip

--- Scripts/floor_generation.gd
+++ Scripts/floor_generation.gd
@@ -1,25 +1,67 @@
 @tool
-extends Node3D
+extends MeshInstance2D
 
-var block = preload("res://Scenes/gen_cube.tscn")
-@export var grid_size: int:
-	set(new_grid_size):
-		grid_size = max(new_grid_size, 0)
+@export var floor_size: int:
+	set(new_floor_size):
+		floor_size = max(new_floor_size, 0)
 		if Engine.is_editor_hint():
-			size_update()
+			gen_floor()
 
 func _ready() -> void:
-	size_update()
-func size_update():
-	if get_children():
-		for iter_block in get_children():
-			iter_block.queue_free()
-	for x in range(grid_size):
-		for z in range(grid_size):
-			var temp_block = block.instantiate()
-			temp_block.position.x = x * 2 - grid_size + 1
-			temp_block.position.z = z * 2 - grid_size + 1
-			temp_block.position.y = sin((temp_block.position.x + temp_block.position.z) / 20)
-			temp_block.name = "Block" + str(grid_size) + str(x * grid_size + z)
-			add_child(temp_block)
-			temp_block.owner = get_tree().edited_scene_root
+	gen_floor()
+
+func gen_floor():
+	var rings = 50
+	var radial_segments = 50
+	var radius = 1
+	var surface_array = []
+	surface_array.resize(Mesh.ARRAY_MAX)
+
+	# PackedVector**Arrays for mesh construction.
+	var verts = PackedVector3Array()
+	var uvs = PackedVector2Array()
+	var normals = PackedVector3Array()
+	var indices = PackedInt32Array()
+
+	var thisrow = 0
+	var prevrow = 0
+	var point = 0
+
+	for i in range(rings + 1):
+		var v = float(i) / rings
+		var w = sin(PI * v)
+		var y = cos(PI * v)
+
+		# Loop over segments in ring.
+		for j in range(radial_segments + 1):
+			var u = float(j) / radial_segments
+			var x = sin(u * PI * 2.0)
+			var z = cos(u * PI * 2.0)
+			var vert = Vector3(x * radius * w, y * radius, z * radius * w)
+			verts.append(vert)
+			normals.append(vert.normalized())
+			uvs.append(Vector2(u, v))
+			point += 1
+
+			# Create triangles in ring using indices.
+			if i > 0 and j > 0:
+				indices.append(prevrow + j - 1)
+				indices.append(prevrow + j)
+				indices.append(thisrow + j - 1)
+
+				indices.append(prevrow + j)
+				indices.append(thisrow + j)
+				indices.append(thisrow + j - 1)
+
+		prevrow = thisrow
+		thisrow = point
+
+	# Assign arrays to surface array.
+	surface_array[Mesh.ARRAY_VERTEX] = verts
+	surface_array[Mesh.ARRAY_TEX_UV] = uvs
+	surface_array[Mesh.ARRAY_NORMAL] = normals
+	surface_array[Mesh.ARRAY_INDEX] = indices
+
+	# Create mesh surface from mesh array.
+	# No blendshapes, lods, or compression used.
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
