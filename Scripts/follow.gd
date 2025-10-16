b85d1febd6fc7a89cb23c7a771a7ee7102209007
n0o0b090lv <ernestspursw@gmail.com>
2025-10-16 23:20:51 +0300

    Making blass trough

--- Scripts/follow.gd
+++ Scripts/follow.gd
@@ -8,8 +8,11 @@ func _ready() -> void:
 	track_object = get_parent()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(track_object.global_position + Vector3.UP * 1)
+	look_at(track_object.global_position + Vector3.UP)
 	var track_position = track_object.global_position + Vector3.UP
 	nb_global_position += (track_position - nb_global_position) * delta * min(max(nb_global_position.distance_to(track_position) - stalk_distance, 0), 1) * 4
-	nb_global_position.y += (track_position.y - nb_global_position.y - 0.3) * delta
+	if track_object.is_on_floor():
+		nb_global_position.y += (track_position.y - nb_global_position.y - 0.5) * delta
+	else:
+		nb_global_position.y += (track_position.y - nb_global_position.y + 1) * delta * 4
 	global_position = nb_global_position
