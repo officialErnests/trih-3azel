9a1c4f87f99c3c287a361d1b136993b1e7e5e86b
n0o0b090lv <ernestspursw@gmail.com>
2025-10-19 20:48:54 +0300

    Music + optimizing

--- Scripts/rail.gd
+++ Scripts/rail.gd
@@ -51,7 +51,7 @@ func _ready() -> void:
 			visual_boxes.add_child(support_box)
 			support_box.scale = Vector3(25, 1000, 25)
 			
-			# for e in range((follow_point.transform.basis.y * 20 + 50) / 20):
+			print(ceil((follow_point.position.y + 40) / 20))
 			for e in range(ceil((follow_point.position.y + 40) / 20)):
 				var base_support_box = MeshInstance3D.new()
 				base_support_box.mesh = copy_cube.mesh
--- Scripts/warning.gd
+++ Scripts/warning.gd
@@ -1,9 +1,28 @@
 extends Control
 
 
-var main_game = preload("res://Scenes/game.tscn")
+const to_load = "res://Scenes/game.tscn"
+var main_game
 
+func _ready() -> void:
+	$Progression.text = "GET THE SHIT..."
+	main_game = ResourceLoader.load_threaded_request(to_load)
+	$Progression.text = "GOT THE SHIT..."
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Jump"):
-		get_tree().change_scene_to_packed(main_game)
+	var progression = []
+	var status = ResourceLoader.load_threaded_get_status(to_load, progression)
+	match status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			$Progression.text = "{" + str(progression[0]) + "} LOADING CRAZY.XDD"
+		ResourceLoader.THREAD_LOAD_LOADED:
+			$Progression.text = "CRAZY.XDD LOADED [SPACE]"
+			if Input.is_action_just_pressed("Jump"):
+				get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(to_load))
+		ResourceLoader.THREAD_LOAD_FAILED:
+			$Progression.text = "FAILED TO LOAD ;-;"
+		ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+			$Progression.text = "AHHHHHHHHHHHH"
+		_:
+			$Progression.text = "Yeah.. idk what happened XD"
