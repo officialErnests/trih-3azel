2f4bc7377ae116cf454b540733c508b3e6e01250
n0o0b090lv <ernestspursw@gmail.com>
2025-10-19 14:38:08 +0300

    Some more adjustments :DD

--- Scripts/close_tutorial.gd
+++ Scripts/close_tutorial.gd
@@ -1,38 +1,36 @@
 extends CanvasLayer
 
-
+var closing = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(5).timeout
-	$TextShadow6.queue_free()
-	await get_tree().create_timer(0.8).timeout
-	$TextShadow5.queue_free()
-	await get_tree().create_timer(0.8).timeout
-	$TextShadow4.queue_free()
-	await get_tree().create_timer(0.8).timeout
-	$TextShadow3.queue_free()
-	await get_tree().create_timer(0.8).timeout
-	$TextShadow2.queue_free()
-	await get_tree().create_timer(0.8).timeout
-	$TextShadow.queue_free()
-	await get_tree().create_timer(0.8).timeout
-	$Text.queue_free()
-	queue_free()
+	await get_tree().create_timer(20).timeout
+	close()
 
 var a = 0
 func _process(delta: float) -> void:
 	a += delta
 	var b = 0
 	if Input.is_action_just_pressed("Jump"):
-		$TextShadow6.queue_free()
-		$TextShadow5.queue_free()
-		$TextShadow4.queue_free()
-		$TextShadow3.queue_free()
-		$TextShadow2.queue_free()
-		$TextShadow.queue_free()
-		$Text.queue_free()
-		queue_free()
+		close()
 
 	for child in get_children():
 		b += 1
 		child.position.x += sin(a * 2 + b / 2.0) * 0.1
+
+func close():
+	if closing: return
+	closing = true
+	$TextShadow6.queue_free()
+	await get_tree().create_timer(0.1).timeout
+	$TextShadow5.queue_free()
+	await get_tree().create_timer(0.1).timeout
+	$TextShadow4.queue_free()
+	await get_tree().create_timer(0.1).timeout
+	$TextShadow3.queue_free()
+	await get_tree().create_timer(0.1).timeout
+	$TextShadow2.queue_free()
+	await get_tree().create_timer(0.1).timeout
+	$TextShadow.queue_free()
+	await get_tree().create_timer(0.1).timeout
+	$Text.queue_free()
+	queue_free()
